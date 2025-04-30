441159ea13a1c81d131689f8741f04d580aed326
qhoncheh <ghoncheh.ataee@gmail.com>
2025-04-30 20:45:32 +0330

    loading added

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
+
 export default function Loading() {
   return (
-    <div>
-      <h1>Loading......</h1>
+    <div className=" pt-40 flex justify-center item-center min-h-[20rem]">
+      <Image
+        width={150}
+        height={150}
+        alt="loading"
+        src={"/loading/loading.svg"}
+      />
     </div>
   );
 }
